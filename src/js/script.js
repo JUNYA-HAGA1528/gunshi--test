9b658e89a88c497dba5c744d584dd07b35a2d056
junyahaga <n0812034@yahoo.co.jp>
2024-03-10 23:23:27 +0900

    2024/0310

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+    $(".frature__en-js").arctext({
+        radius : 50,
+    });
+
+    $(".head-title-js").arctext({
+        radius : 50,
+    });
+
+});
